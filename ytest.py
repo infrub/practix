aaee300e493b0631135381be103c58cd18ef3348
infrub <infrub@gmail.com>
2020-06-28 17:08:29 +0900

    switch_ashi

--- ytest.py
+++ ytest.py
@@ -63,19 +63,21 @@ for ashi in ashis:
     fig.delaxes(candle_axs[ashi])
 
 
+rexs = {"m05":yoyuu}
+lexs = {"m05":max(0,yoyuu-flamesize)}
 
+for ashi in ashis:
+    if ashi=="m05": continue
+    rex = dfs["m05"]["matching_closeX_in_"+ashi][rexs["m05"]-1]
+    rexs[ashi] = rex
+    lexs[ashi] = max(0,rex-flamesize)
 
-
-rex_in_m05 = yoyuu
-lex_in_m05 = max(0, rex_in_m05 - flamesize)
 watching_ashi = "h01"
 
 
-def create_aax(ashi):
-    mac_main = dfs[ashi].macd_main
-    mac_signal = dfs[ashi].macd_signal
-    mac_axs[ashi].plot(idxs[ashi]+1, mac_main, linewidth = lw) #close時点で値がでるのでこうして1シフトしておくのが正しい(だよね？確認してない) #TODO
-    mac_axs[ashi].plot(idxs[ashi]+1, mac_signal, linewidth = lw)
+def create_ax(ashi):
+    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_main, linewidth = lw) #close時点で値がでるのでこうして1シフトしておくのが正しい(だよね？確認してない) #TODO
+    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_signal, linewidth = lw)
     mac_axs[ashi].grid(True,linestyle='dotted')
     mac_axs[ashi].tick_params(labelbottom=False)
 
@@ -86,41 +88,77 @@ def create_aax(ashi):
     candle_axs[ashi].set_xticks(np.arange(0,len(dfs[ashi]),trip))
     candle_axs[ashi].set_xticklabels(dfs[ashi].openTime[0:len(dfs[ashi]):trip].dt.strftime('%Y-%m-%d\n%H:%M'),rotation=0,size="small")
 
-    candle_axs[ashi].set_xlim(lex_in_m05,rex_in_m05)
+    candle_axs[ashi].set_xlim(lexs[ashi],rexs[ashi])
 
-    yhani = dfs[ashi].closePrice[lex_in_m05:rex_in_m05]
+    yhani = dfs[ashi].closePrice[lexs[ashi]:rexs[ashi]]
     candle_axs[ashi].grid(True,linestyle='dotted')
 
-
-
-def move_aax(new_rex_in_m05):
-    global rex_in_m05, lex_in_m05
-    rex_in_m05 = new_rex_in_m05
-    lex_in_m05 = max(0,rex_in_m05-flamesize)
-
-    ashi = "m05"
-    candle_axs[ashi].set_xlim(lex_in_m05,rex_in_m05)
+def move_tick_with_new_rex_in_m05(new_rex_in_m05):
+    for ashi in ashis:
+        rex = dfs["m05"]["matching_closeX_in_"+ashi][new_rex_in_m05-1]
+        rexs[ashi] = rex
+        lexs[ashi] = max(0,rex-flamesize)
+        candle_axs[ashi].set_xlim(lexs[ashi],rexs[ashi])
     plt.draw()
 
-def next_aax(event):
-    move_aax(rex_in_m05+1)
+def next_tick(event):
+    move_tick_with_new_rex_in_m05(rexs["m05"]+1)
 
-def prev_aax(event):
-    move_aax(rex_in_m05-1)
+def prev_tick(event):
+    move_tick_with_new_rex_in_m05(rexs["m05"]-1)
+
+
+def get_func_of_switch_ashi(ashi):
+    global watching_ashi
+    def switch(event):
+        global watching_ashi
+        fig.delaxes(mac_axs[watching_ashi])
+        fig.delaxes(candle_axs[watching_ashi])
+        watching_ashi = ashi
+        fig.add_axes(mac_axs[watching_ashi])
+        fig.add_axes(candle_axs[watching_ashi])
+        plt.draw()
+    return switch
+
+
+
+
+
+btn_m01 = Button(plt.axes([0.33, 0.04, 0.1, 0.035]), 'm01',color = 'black')
+btn_m01.on_clicked(get_func_of_switch_ashi("m01"))
+#btn_m05 = Button(plt.axes([0.44, 0.04, 0.1, 0.035]), 'm05',color = 'black')
+#btn_m05.on_clicked(callback.get_func_of_switch_ashi("m05"))
+btn_m15 = Button(plt.axes([0.55, 0.04, 0.1, 0.035]), 'm15',color = 'black')
+btn_m15.on_clicked(get_func_of_switch_ashi("m15"))
+
+btn_h01 = Button(plt.axes([0.33, 0.00, 0.1, 0.035]), 'h01',color = 'black')
+btn_h01.on_clicked(get_func_of_switch_ashi("h01"))
+btn_h04 = Button(plt.axes([0.44, 0.00, 0.1, 0.035]), 'h04',color = 'black')
+btn_h04.on_clicked(get_func_of_switch_ashi("h04"))
+btn_d01 = Button(plt.axes([0.55, 0.00, 0.1, 0.035]), 'd01',color = 'black')
+btn_d01.on_clicked(get_func_of_switch_ashi("d01"))
+
 
 
 
 btn_prev = Button(plt.axes([0.7, 0.0, 0.1, 0.075]), 'Prev',color = 'black')
-btn_prev.on_clicked(prev_aax)
+btn_prev.on_clicked(prev_tick)
 btn_next = Button(plt.axes([0.81, 0.0, 0.1, 0.075]), 'Next',color = 'black')
-btn_next.on_clicked(next_aax)
+btn_next.on_clicked(next_tick)
+
 
 
 
 ashi = "m05"
 fig.add_axes(mac_axs[ashi])
 fig.add_axes(candle_axs[ashi])
-create_aax(ashi)
+
+ashi = watching_ashi
+fig.add_axes(mac_axs[ashi])
+fig.add_axes(candle_axs[ashi])
+
+for ashi in ashis:
+    create_ax(ashi)
 
 
 
