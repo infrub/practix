6b4e909372a9e87326558d4406f3e824940f20bb
infrub <infrub@gmail.com>
2020-06-30 14:02:26 +0900

    show_pdf kansei

--- show_pdf.py
+++ show_pdf.py
@@ -56,21 +56,23 @@ for ashi in ashis:
     idxs[ashi] = np.arange(len(dfs[ashi].index))
 
 order_df = pd.read_csv("logs/" + pairname + "/week_" + str(weeki).zfill(3) + ".csv", parse_dates=True)
+order_df["profitPipsPer5Min"] = order_df.profitPips / (order_df.exitX - order_df.entryX)
 buy_order_df = order_df[order_df.entryStatus==1]
 sell_order_df = order_df[order_df.entryStatus==-1]
 
 #styles
 priceLineWidth = 0.6
 bgcolor = "#131313"
-plt.style.use('dark_background')
 
 
 candle_axs = {ashi:None for ashi in ashis}
 mac_axs = {ashi:None for ashi in ashis}
+gain_axs = {ashi:None for ashi in ashis}
 
 #(left,bottom,width,height)
 acax_position = (0.05,0.33,0.9,0.6) # A面のcandle axの位置
 amax_position = (0.05,0.13,0.9,0.2)
+agax_position = (0.05,0.03,0.9,0.1)
 
 rexs = {}
 lexs = {}
@@ -80,29 +82,56 @@ lexs = {}
 
 def create_ax(ashi):
     mpf.candlestick2_ohlc_indexed_by_openTime(candle_axs[ashi], dfs[ashi].openPrice, dfs[ashi].highPrice, dfs[ashi].lowPrice, dfs[ashi].closePrice, width=0.7, alpha=1.0, colorup="#fa2200", colordown="#0077ff")
-    candle_axs[ashi].scatter(idxs[ashi]+1, dfs[ashi].MA_short, color="#f6ad48", s=1) # 終値の単純移動平均(ピリオド5)
-    candle_axs[ashi].scatter(idxs[ashi]+1, dfs[ashi].MA_mid, color="#aacf52", s=1) # 終値の単純移動平均(ピリオド13)
-    candle_axs[ashi].scatter(idxs[ashi]+1, dfs[ashi].MA_long, color="#00b1a9", s=1) # 終値の単純移動平均(ピリオド25)
+    candle_axs[ashi].scatter(idxs[ashi]+1, dfs[ashi].MA_short, color="#f6ad48", s=1, label="SAR(5)") # 終値の単純移動平均(ピリオド5)
+    candle_axs[ashi].scatter(idxs[ashi]+1, dfs[ashi].MA_mid, color="#aacf52", s=1, label="SAR(13)") # 終値の単純移動平均(ピリオド13)
+    candle_axs[ashi].scatter(idxs[ashi]+1, dfs[ashi].MA_long, color="#00b1a9", s=1, label="SAR(25)") # 終値の単純移動平均(ピリオド25)
     candle_axs[ashi].set_xticks(np.arange(0,len(dfs[ashi]),trip))
     candle_axs[ashi].set_xticklabels(dfs[ashi].openTime[0:len(dfs[ashi]):trip].dt.strftime('%Y-%m-%d\n%H:%M'),rotation=0,size="small")
 
-    candle_axs[ashi].scatter(buy_order_df.entryX+0.25, buy_order_df.entryPrice,s=24,c='#ffbbbb',marker='>',label="Buy Entry")
-    candle_axs[ashi].scatter(buy_order_df.exitX-0.25, buy_order_df.exitPrice,s=24,c='#ffbbbb',marker='<', label="Buy Settle")
-    candle_axs[ashi].scatter(sell_order_df.entryX+0.25, sell_order_df.entryPrice,s=24,c='#bbbbff',marker='>', label="Sell Entry")
-    candle_axs[ashi].scatter(sell_order_df.exitX-0.25, sell_order_df.exitPrice,s=24,c='#bbbbff',marker='<', label="Sell Settle")
+    candle_axs[ashi].scatter(buy_order_df.entryX+0.25, buy_order_df.entryPrice,s=24,c='#ffbbbb',marker='>', label="INVIS_LABEL")
+    candle_axs[ashi].scatter(buy_order_df.exitX-0.25, buy_order_df.exitPrice,s=24,c='#ffbbbb',marker='<', label="INVIS_LABEL")
+    candle_axs[ashi].scatter(sell_order_df.entryX+0.25, sell_order_df.entryPrice,s=24,c='#bbbbff',marker='>', label="INVIS_LABEL")
+    candle_axs[ashi].scatter(sell_order_df.exitX-0.25, sell_order_df.exitPrice,s=24,c='#bbbbff',marker='<', label="INVIS_LABEL")
 
 
-    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_main, color="mediumvioletred", linewidth = 1.5) # MACD(5,11,4) main
-    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_signal, color="lightslategrey", linewidth = 1.5) # MACD(5,11,4) sig
+    for orderi, order in buy_order_df.iterrows():
+        mac_axs[ashi].axvspan(order.entryX, order.exitX, color="#552222")
+    for orderi, order in sell_order_df.iterrows():
+        mac_axs[ashi].axvspan(order.entryX, order.exitX, color="#222255")
+
+    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_signal, color="lightslategrey", linewidth = 1.5, label="MACD(5,11,4) main") # MACD(5,11,4) sig
+    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_main, color="mediumvioletred", linewidth = 1.5, label="MACD(5,11,4) sig") # MACD(5,11,4) main
     mac_axs[ashi].grid(True,linestyle='dotted')
     mac_axs[ashi].tick_params(labelbottom=False)
 
-    for i in range(len(buy_order_df)):
-        mac_axs[ashi].axvspan(buy_order_df.iloc[i].entryX, buy_order_df.iloc[i].exitX, color="#552222")
-    for i in range(len(sell_order_df)):
-        mac_axs[ashi].axvspan(sell_order_df.iloc[i].entryX, sell_order_df.iloc[i].exitX, color="#222255")
 
+    for orderi, order in buy_order_df.iterrows():
+        gain_axs[ashi].axvspan(order.entryX, order.exitX, color="#552222")
+    for orderi, order in sell_order_df.iterrows():
+        gain_axs[ashi].axvspan(order.entryX, order.exitX, color="#222255")
 
+    colors = []
+    for orderi, order in order_df.iterrows():
+        if order.profitPips < 0:
+            colors.append("fuchsia") #"#b70868")
+        else:
+            colors.append("lime") #"#00a161")
+
+    gain_axs[ashi].bar(order_df.entryX.values, order_df.profitPipsPer5Min, width=order_df.exitX-order_df.entryX, color=colors, align="edge")
+    annoy = 999
+    for orderi, order in order_df.iterrows():
+        if order.profitPipsPer5Min < 0:
+            annoy = 0.5
+        else:
+            annoy = -1.8
+        gain_axs[ashi].annotate(f"{order.profitPips:+.1f}", ((order.entryX+order.exitX)/2-1, annoy), size=8, rotation=25)
+
+    gain_axs[ashi].grid(True,linestyle='dotted')
+    gain_axs[ashi].set_ylim(-3,3)
+    gain_axs[ashi].set_ylabel("profit pips")
+    gain_axs[ashi].set_yticks([0])
+    gain_axs[ashi].set_yticklabels([""])
+    gain_axs[ashi].tick_params(labelbottom=False)
 
 
     candle_axs[ashi].set_xlim(lexs[ashi],rexs[ashi])
@@ -115,25 +144,81 @@ def create_ax(ashi):
     candle_axs[ashi].grid(True,linestyle='dotted')
 
 
+    handlers1, labels1 = candle_axs[ashi].get_legend_handles_labels()
+    handlers2, labels2 = mac_axs[ashi].get_legend_handles_labels()
+    handlers3, labels3 = gain_axs[ashi].get_legend_handles_labels()
+    handlers = []
+    labels = []
+    for handler,label in zip(handlers1 + handlers2 + handlers3, labels1 + labels2 + labels3):
+        if label != "INVIS_LABEL":
+            handlers.append(handler)
+            labels.append(label)
+    candle_axs[ashi].legend(handlers, labels, borderaxespad=0.)
+
+
+
+
+def plot_top_page():
+    all_len = len(order_df)
+    all_score = np.sum(order_df.profitPips)
+
+    won_order_df = order_df[order_df.profitPips>0]
+    won_len = len(won_order_df)
+    won_score = np.sum(won_order_df.profitPips)
+
+    loss_order_df = order_df[order_df.profitPips<0]
+    loss_len = len(loss_order_df)
+    loss_score = np.sum(loss_order_df.profitPips)
+
+    draw_order_df = order_df[order_df.profitPips==0]
+    draw_len = len(draw_order_df)
+    draw_score = np.sum(draw_order_df.profitPips)
+
+    rowLabels = ["All","Won","Lost","Draw"]
+    colLabels = ["Number", "Sum pips", "Mean pips"]
+    cellTextss = [[f"{ all_len:>5}", f"{all_score:>9.2f}", f"{all_score/all_len:>8.3f}"],
+                [f"{ won_len:>5}({int( won_len/all_len*100):>2}%)", f"{ won_score:>9.2f}", f"{ won_score/ won_len:>8.3f}"],
+                [f"{loss_len:>5}({int(loss_len/all_len*100):>2}%)", f"{loss_score:>9.2f}", f"{loss_score/loss_len:>8.3f}"],
+                [f"{draw_len:>5}({int(draw_len/all_len*100):>2}%)", f"{draw_score:>9.2f}", f"{draw_score/draw_len:>8.3f}"]]
+    colWidths = [0.2,0.2,0.2]
+
+
+    fig = plt.figure(figsize=(13,8))
+
+    plt.gca().spines['left'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['bottom'].set_visible(False)
+    plt.gca().spines['top'].set_visible(False)
+    plt.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False)
+    plt.tick_params(bottom=False, left=False, right=False, top=False)
+    plt.text(0,0.8, pairname+" week_"+str(weeki).zfill(3), fontsize=30)
+    table = plt.table(cellText=cellTextss, rowLabels=rowLabels, colLabels=colLabels, colWidths=colWidths, bbox=[0.1,0.0,0.8,0.7], loc="center")
+    table.auto_set_font_size(False)
+    table.set_fontsize(30)
+
+
+
+
 
 def plot_and_save_all_page():
     pdf_filename = "logs/" + pairname + "/week_" + str(weeki).zfill(3) + ".pdf"
     pdf = PdfPages(pdf_filename)
 
-    """
+    plt.style.use('default')
     plot_top_page()
     pdf.savefig()
     plt.close()
-    """
 
     ashi = "m05"
 
     for lex in trange(0, len(dfs[ashi]), flamesize):
+        plt.style.use('dark_background')
         lexs[ashi] = lex
         rexs[ashi] = min(len(dfs[ashi]), lex+flamesize)
-        fig = plt.figure(figsize=(12,7), num=f"{pairname} week_"+ str(weeki).zfill(3), facecolor=bgcolor)
+        fig = plt.figure(figsize=(13,8), num=pairname+" week_"+str(weeki).zfill(3), facecolor=bgcolor)
         mac_axs[ashi] = fig.add_axes(amax_position, facecolor=bgcolor)
         candle_axs[ashi] = fig.add_axes(acax_position,sharex=mac_axs[ashi], facecolor=bgcolor)
+        gain_axs[ashi] = fig.add_axes(agax_position, sharex=mac_axs[ashi], facecolor=bgcolor)
         create_ax(ashi)
         pdf.savefig()
         plt.close()
--- ytest.py
+++ ytest.py
@@ -110,17 +110,19 @@ sumProfit = 0
 
 
 def create_ax(ashi):
-    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_main, color="mediumvioletred", linewidth = 1.5) # MACD(5,11,4) main
-    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_signal, color="lightslategrey", linewidth = 1.5) # MACD(5,11,4) sig
-    mac_axs[ashi].grid(True,linestyle='dotted')
-    mac_axs[ashi].tick_params(labelbottom=False)
-
     mpf.candlestick2_ohlc_indexed_by_openTime(candle_axs[ashi], dfs[ashi].openPrice, dfs[ashi].highPrice, dfs[ashi].lowPrice, dfs[ashi].closePrice, width=0.7, alpha=1.0, colorup="#fa2200", colordown="#0077ff")
     candle_axs[ashi].scatter(idxs[ashi]+1, dfs[ashi].MA_short, color="#f6ad48", s=1) # 終値の単純移動平均(ピリオド5)
     candle_axs[ashi].scatter(idxs[ashi]+1, dfs[ashi].MA_mid, color="#aacf52", s=1) # 終値の単純移動平均(ピリオド13)
     candle_axs[ashi].scatter(idxs[ashi]+1, dfs[ashi].MA_long, color="#00b1a9", s=1) # 終値の単純移動平均(ピリオド25)
     candle_axs[ashi].set_xticks(np.arange(0,len(dfs[ashi]),trip))
     candle_axs[ashi].set_xticklabels(dfs[ashi].openTime[0:len(dfs[ashi]):trip].dt.strftime('%Y-%m-%d\n%H:%M'),rotation=0,size="small")
+
+    
+    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_signal, color="lightslategrey", linewidth = 1.5) # MACD(5,11,4) sig
+    mac_axs[ashi].plot(idxs[ashi]+1, dfs[ashi].macd_main, color="mediumvioletred", linewidth = 1.5) # MACD(5,11,4) main
+    mac_axs[ashi].grid(True,linestyle='dotted')
+    mac_axs[ashi].tick_params(labelbottom=False)
+
 
     candle_axs[ashi].set_xlim(lexs[ashi],rexs[ashi])
     miny = min(dfs[ashi].lowPrice[lexs[ashi]:rexs[ashi]])
